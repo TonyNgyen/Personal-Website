94e3a79b73e3fdf2473f388f7e6289289ee83d44
Tony Nguyen <tonynguyeenn@gmail.com>
2023-09-18 22:16:38 -0700

    This is a test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='flex justify-between'>
+        <h1 className='w-1/2 bg-slate-200'>Tony Nguyen</h1>
+        <ul className='flex justify-around w-1/3'>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
 
 export default App;
+
+// https://huemint.com/website-1/#palette=fbf9f6-042e38-d02622
+// https://coolors.co/2c363f-a41623-f2f5ea-f85e00-918450
+
+// Test
