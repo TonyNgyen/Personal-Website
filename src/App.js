c210a64e240b59b6ea9dd6a3c1fb475d0f4de983
Tony Nguyen <tonynguyeenn@gmail.com>
2023-09-20 01:27:23 -0700

    Added header

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App bg-white">
-      <nav className='flex justify-between py-10 px-20'>
+    <div className="App h-screen">
+      <nav className='flex justify-between py-8 px-20 sticky top-0 bg-white'>
         <h1 className=' text-4xl font-montserrat font-semibold'>Tony Nguyen</h1>
-        <ul className='flex justify-around w-1/3 text-xl font-montserrat'>
+        <ul className='flex justify-around w-1/3 text-xl font-montserrat pt-1'>
           <li><a href="#">About</a></li>
           <li><a href="#">Projects</a></li>
           <li><a href="#">Contact Me</a></li>
         </ul>
       </nav>
-      <header>
-        
+      <header className='bg-Snow h-5/6 flex justify-center items-center'>
+        <article>
+          <h1 className=''>
+            Hey, I'm Tony
+          </h1>
+        </article>
       </header>
     </div>
   );
