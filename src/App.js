019890ad6f948f0780c902d21e3fd26685aa9773
Tony Nguyen <tonynguyeenn@gmail.com>
2023-09-20 21:31:46 -0700

    Added more to header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import { Cursor, Typewriter, useTypewriter } from 'react-simple-typewriter'
 
 function App() {
+  const [text, count] = useTypewriter({
+    words: ["Tony", "a software engineer"],
+    loop: true,
+    delaySpeed: 2000,
+  })
   return (
     <div className="App h-screen">
       <nav className='flex justify-between py-8 px-20 sticky top-0 bg-white'>
@@ -11,11 +17,18 @@ function App() {
           <li><a href="#">Contact Me</a></li>
         </ul>
       </nav>
-      <header className='bg-Snow h-5/6 flex justify-center items-center'>
-        <article>
-          <h1 className=''>
-            Hey, I'm Tony
+      <header className='bg-Snow h-5/6 flex justify-center items-center font-montserrat'>
+        <article className=''>
+          <h1 className='text-7xl font-semibold text-center mb-10'>
+            <span className=''>
+              Hey, I'm {text}
+            </span>
+            <Cursor cursorColor='#A41623'></Cursor>
           </h1>
+          <h2 className='w-3/5 text-center mx-auto'>
+          A software engineer expanding his skills and building fullstack 
+          applications and websites that are useful to others and interest me.
+          </h2>
         </article>
       </header>
     </div>
