9c0dbbf466dc7b57480cc15f6be99eb6c3cfa04d
Tony Nguyen <tonynguyeenn@gmail.com>
2023-10-01 20:44:13 -0700

    Reformatted files

--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,7 @@ import emailjs from "@emailjs/browser";
 import React, { useRef } from "react";
 
 function Contact() {
-    const form = useRef();
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -26,54 +26,52 @@ function Contact() {
   };
   return (
     <section className="bg-gray-100 pt-40 pb-40 min-h-screen" id="contact">
-        <article className="font-montserrat">
-          <h2 className="text-5xl font-semibold mb-6 text-center">
-            Contact Me
-          </h2>
-          <div className="w-10 bg-Ruby h-2 mx-auto mb-6 rounded-md"></div>
-          <h2 className="text-xl mx-auto w-5/12 mb-20 text-center">
-            If you want to talk about an opportunity or just want to talk to me about anything,
-            please do not hesitate to contact me!
-          </h2>
-          <form
-            ref={form}
-            onSubmit={sendEmail}
-            className="bg-Snow w-5/12 mx-auto px-10 py-8 drop-shadow-2xl rounded-lg"
-          >
-            <label className="block mb-2 font-bold">Name</label>
-            <input
-              type="text"
-              name="user_name"
-              className="block mb-8 bg-gray-200 w-full p-4 rounded-md drop-shadow-sm"
-              placeholder="Enter Your Name"
-            />
-            <label className="block mb-2 font-bold">Email</label>
-            <input
-              type="email"
-              name="user_email"
-              className="block mb-8 bg-gray-200 w-full p-4 rounded-md drop-shadow-sm"
-              placeholder="Enter Your Email"
-            />
-            <label className="block mb-2 font-bold">Message</label>
-            <textarea
-              name="message"
-              placeholder="Enter Your Message"
-              rows="8"
-              className="block bg-gray-200 mb-8 w-full resize-none rounded-md drop-shadow-sm p-4 h-auto"
-            />
-            <div className="flex justify-end">
-              <button className="hover:cursor-pointer bg-Ruby rounded-md">
-                <input
-                  type="submit"
-                  value="Send"
-                  className="hover:cursor-pointer text-white font-bold text-xl py-3 px-10"
-                />
-              </button>
-            </div>
-          </form>
-        </article>
-      </section>
-  )
+      <article className="font-montserrat">
+        <h2 className="text-5xl font-semibold mb-6 text-center">Contact Me</h2>
+        <div className="w-10 bg-Ruby h-2 mx-auto mb-6 rounded-md"></div>
+        <h2 className="text-xl mx-auto w-5/12 mb-20 text-center">
+          If you want to talk about an opportunity or just want to talk to me
+          about anything, please do not hesitate to contact me!
+        </h2>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="bg-Snow w-5/12 mx-auto px-10 py-8 drop-shadow-2xl rounded-lg"
+        >
+          <label className="block mb-2 font-bold">Name</label>
+          <input
+            type="text"
+            name="user_name"
+            className="block mb-8 bg-gray-200 w-full p-4 rounded-md drop-shadow-sm"
+            placeholder="Enter Your Name"
+          />
+          <label className="block mb-2 font-bold">Email</label>
+          <input
+            type="email"
+            name="user_email"
+            className="block mb-8 bg-gray-200 w-full p-4 rounded-md drop-shadow-sm"
+            placeholder="Enter Your Email"
+          />
+          <label className="block mb-2 font-bold">Message</label>
+          <textarea
+            name="message"
+            placeholder="Enter Your Message"
+            rows="8"
+            className="block bg-gray-200 mb-8 w-full resize-none rounded-md drop-shadow-sm p-4 h-auto"
+          />
+          <div className="flex justify-end">
+            <button className="hover:cursor-pointer bg-Ruby rounded-md">
+              <input
+                type="submit"
+                value="Send"
+                className="hover:cursor-pointer text-white font-bold text-xl py-3 px-10"
+              />
+            </button>
+          </div>
+        </form>
+      </article>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
