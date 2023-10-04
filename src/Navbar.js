639563e373347d4ca2a806de7379a869d25cd531
Tony Nguyen <tonynguyeenn@gmail.com>
2023-10-04 00:27:39 -0700

    Added working navigation for small and medium devices

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 
 function Navbar() {
+  let [open, setOpen] = useState(true);
   return (
-    <nav className="flex justify-between py-7 px-20 sticky top-0 bg-white z-20 shadow-md">
-      <h1 className=" text-4xl font-montserrat font-bold">
+    <nav className="md:flex md:justify-between py-7 md:px-20 px-5 sticky top-0 bg-white z-20 shadow-md">
+      <h1 className=" md:text-4xl font-montserrat font-bold text-3xl pl-3">
         <a href="#home">Tony Nguyen</a>
       </h1>
-      <ul className="flex justify-around w-1/3 text-xl font-montserrat pt-1 font-semibold">
-        <li>
-          <a href="#about" className="hover:text-Ruby hover:underline underline-offset-8">About</a>
+      <div>
+        <button
+          className="text-3xl absolute right-8 top-8 cursor-pointer md:hidden"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <HiMenu /> : <HiX />}
+        </button>
+      </div>
+      <ul
+        className={
+          "md:flex md:justify-around lg:w-1/3 text-xl font-montserrat md:pt-1 font-semibold absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:pl-0 pl-8 transition-opacity duration-500 ease-in md:opacity-100 " +
+          (open ? "opacity-0" : "opacity-100")
+        }
+      >
+        <li className="md:my-0 mt-5">
+          <a
+            href="#about"
+            className="hover:text-Ruby hover:underline underline-offset-8"
+          >
+            About
+          </a>
         </li>
-        <li>
-          <a href="#projects" className="hover:text-Ruby hover:underline underline-offset-8">Projects</a>
+        <li className="md:my-0 mt-5">
+          <a
+            href="#projects"
+            className="hover:text-Ruby hover:underline underline-offset-8"
+          >
+            Projects
+          </a>
         </li>
-        <li>
-          <a href="#contact" className="hover:text-Ruby hover:underline underline-offset-8">Contact</a>
+        <li className="md:my-0 mt-5 mb-8">
+          <a
+            href="#contact"
+            className="hover:text-Ruby hover:underline underline-offset-8"
+          >
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
